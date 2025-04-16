ba005a41c85ab2b4d70aa6f0098104d2ff25f133
Andreas Lordos <andreasglordos@gmail.com>
2025-04-16 15:14:34 -0400

    got rid of 10pm

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -21,7 +21,6 @@ const Calendar = () => {
       { hour: 7, meridiem: 'pm' },
       { hour: 8, meridiem: 'pm' },
       { hour: 9, meridiem: 'pm' },
-      { hour: 10, meridiem: 'pm' },
     ];
     
     // Days of the week
